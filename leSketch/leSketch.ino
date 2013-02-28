34bdd9df3040aae473d130e8bf3519831505330b
Ilovebugs <nd.moreau@gmail.com>
2013-02-28 17:45:30 +0100

    2013-02-28

--- leSketch/leSketch.ino
+++ leSketch/leSketch.ino
@@ -1,4 +1,6 @@
 #include <Wire.h>
+#include "NDEFPushProtocol.h"
+#include "NFCLinkLayer.h"
 #include "PN532.h"
 
 uint8_t _irq = 2;
@@ -6,13 +8,35 @@ uint8_t _reset = 3;
 boolean debug = true;
 
 PN532 nfc(_irq,_reset);
+NFCLinkLayer linkLayer(&nfc);
+NDEFPushProtocol nppLayer(&linkLayer);
+
+// This message shall be used to rx or tx
+// NDEF messages it shall never be released
+#define MAX_PKT_HEADER_SIZE 50
+#define MAX_PKT_PAYLOAD_SIZE 100
+uint8_t rxNDEFMessage[MAX_PKT_HEADER_SIZE + MAX_PKT_PAYLOAD_SIZE];
+uint8_t txNDEFMessage[MAX_PKT_HEADER_SIZE + MAX_PKT_PAYLOAD_SIZE];
+uint8_t *txNDEFMessagePtr;
+uint8_t *rxNDEFMessagePtr;
+uint8_t txLen;
 
 void setup(void){
 
   Serial.begin(9600);
   Serial.println("Hello!");
 
-  
+  uint8_t message[33] = "01234";                           // This is the message that we want to send
+  txNDEFMessagePtr = &txNDEFMessage[MAX_PKT_HEADER_SIZE]; //txNDEFMessagePtr now points to first byte of the actual message
+  rxNDEFMessagePtr = &rxNDEFMessage[0];                   //rxNDEFMessagePtr now points to the header of rxNDEFMessage[0]
+  txLen = createNDEFShortRecord(message, 5, txNDEFMessagePtr);
+    
+  if (!txLen)
+  {
+      Serial.println("Failed to create NDEF Message.");
+      while(true); //halt
+  }
+    
   nfc.initializeReader();
 
 }
